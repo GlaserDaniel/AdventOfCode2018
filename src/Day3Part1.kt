1a3b07ae62f3055490f5b32761127733c8493a74
Daniel Glaser <danielglaser9@gmail.com>
2018-12-03 14:07:13 +0100

    Finish Day3Part1

--- src/Day3Part1.kt
+++ src/Day3Part1.kt
@@ -11,7 +11,16 @@ fun day3Part1() {
         input = input.substring(input.indexOf("\n")+1, input.length)
     }
 
-    val fabricArray = arrayListOf<ArrayList<Int>>()
+    var fabricArray = arrayOf<Array<Int>>()
+
+    // initialize with 0
+    for (i in 0..8) {
+        var array = arrayOf<Int>()
+        for (j in 0..8) {
+            array += 0
+        }
+        fabricArray += array
+    }
 
     for (claim in claims) {
         val positionX = Integer.parseInt(claim.substring(claim.indexOf("@")+1, claim.indexOf(",")).trim())
@@ -20,17 +29,24 @@ fun day3Part1() {
         val widthX = Integer.parseInt(claim.substring(claim.indexOf(":")+1, claim.indexOf("x")).trim())
         val heightY = Integer.parseInt(claim.substring(claim.indexOf("x")+1, claim.length))
 
-        for (x in positionX..widthX) {
-            for (y in positionY..heightY) {
-                fabricArray[x][y] += 1
+        for (x in positionX until positionX + widthX) {
+            for (y in positionY until positionY + heightY) {
+                fabricArray[y][x] += 1
             }
-        }
-
-        for (array in fabricArray) {
-            for (value in array) {
-                print(value)
-            }
-            println()
         }
     }
+
+    var result = 0
+
+    for (array in fabricArray) {
+        for (value in array) {
+            //print(value)
+            if (value >= 2) {
+                result++
+            }
+        }
+        //println()
+    }
+
+    println("\nResult: $result")
 }
