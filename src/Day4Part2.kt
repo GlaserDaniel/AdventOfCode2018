52bfcf50d385a9c50f5cc640feba643a36e424c6
Daniel Glaser <danielglaser9@gmail.com>
2018-12-05 12:02:13 +0100

    Added the task description to the functions

--- src/Day4Part2.kt
+++ src/Day4Part2.kt
@@ -1,8 +1,72 @@
+/*
+--- Day 4: Repose Record ---
+You've sneaked into another supply closet - this time, it's across from the prototype suit manufacturing lab. You need to sneak inside and fix the issues with the suit, but there's a guard stationed outside the lab, so this is as close as you can safely get.
+
+As you search the closet for anything that might help, you discover that you're not the first person to want to sneak in. Covering the walls, someone has spent an hour starting every midnight for the past few months secretly observing this guard post! They've been writing down the ID of the one guard on duty that night - the Elves seem to have decided that one guard was enough for the overnight shift - as well as when they fall asleep or wake up while at their post (your puzzle input).
+
+For example, consider the following records, which have already been organized into chronological order:
+
+[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up
+Timestamps are written using year-month-day hour:minute format. The guard falling asleep or waking up is always the one whose shift most recently started. Because all asleep/awake times are during the midnight hour (00:00 - 00:59), only the minute portion (00 - 59) is relevant for those events.
+
+Visually, these records show that the guards are asleep at these times:
+
+Date   ID   Minute
+            000000000011111111112222222222333333333344444444445555555555
+            012345678901234567890123456789012345678901234567890123456789
+11-01  #10  .....####################.....#########################.....
+11-02  #99  ........................................##########..........
+11-03  #10  ........................#####...............................
+11-04  #99  ....................................##########..............
+11-05  #99  .............................................##########.....
+The columns are Date, which shows the month-day portion of the relevant day; ID, which shows the guard on duty that day; and Minute, which shows the minutes during which the guard was asleep within the midnight hour. (The Minute column's header shows the minute's ten's digit in the first row and the one's digit in the second row.) Awake is shown as ., and asleep is shown as #.
+
+Note that guards count as asleep on the minute they fall asleep, and they count as awake on the minute they wake up. For example, because Guard #10 wakes up at 00:25 on 1518-11-01, minute 25 is marked as awake.
+
+If you can figure out the guard most likely to be asleep at a specific time, you might be able to trick that guard into working tonight so you can have the best chance of sneaking in. You have two strategies for choosing the best guard/minute combination.
+
+Strategy 1: Find the guard that has the most minutes asleep. What minute does that guard spend asleep the most?
+
+In the example above, Guard #10 spent the most minutes asleep, a total of 50 minutes (20+25+5), while Guard #99 only slept for a total of 30 minutes (10+10+10). Guard #10 was asleep most during minute 24 (on two days, whereas any other minute the guard was asleep was only seen on one day).
+
+While this example listed the entries in chronological order, your entries are in the order you found them. You'll need to organize them before they can be analyzed.
+
+What is the ID of the guard you chose multiplied by the minute you chose? (In the above example, the answer would be 10 * 24 = 240.)
+
+Your puzzle answer was 99911.
+
+--- Part Two ---
+Strategy 2: Of all guards, which guard is most frequently asleep on the same minute?
+
+In the example above, Guard #99 spent minute 45 asleep more than any other guard or minute - three times in total. (In all other cases, any guard spent any minute asleep at most twice.)
+
+What is the ID of the guard you chose multiplied by the minute you chose? (In the above example, the answer would be 99 * 45 = 4455.)
+
+Your puzzle answer was 65854.
+*/
+
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.collections.HashMap
 
 fun day4Part2() {
     val startTime = System.currentTimeMillis()
@@ -83,27 +147,25 @@ fun day4Part2() {
     // Idee HashMap oder Key Value oder Map
     // [ID] [[minute] [value]]
 
-    // the index of the outer array is the id of the guard
-    // the index of the inner array is the minute
-    // the value of the inner array is how often the guard sleeps in that minute
-    var overlapMinutesArray = arrayOf<Array<Int>>()
+    var overlappingMinutesArray = HashMap<Int, HashMap<Int, Int>>()
 
     // initialize with 0
-    for (i in 0..guards.size) {
-        var array = arrayOf<Int>()
+    for (guard in guards) {
+        overlappingMinutesArray[guard.id] = HashMap(60)
         for (j in 0..60) {
-            array += 0
+            overlappingMinutesArray[guard.id]?.set(j, 0)
         }
-        overlapMinutesArray += array
     }
 
     for (guardInList in guards) {
         for (sleep in guardInList.listOfSleeps) {
-            for ((index, value) in overlapMinutesArray[guardInList.id].withIndex()) {
-                if (index >= sleep.first.minute
-                    && index < sleep.second.minute
+            for (i in 0..60) {
+                if (i >= sleep.first.minute
+                    && i < sleep.second.minute
                 ) {
-                    overlapMinutesArray[guardInList.id][index] += 1
+                    overlappingMinutesArray[guardInList.id]?.set(i,
+                        overlappingMinutesArray[guardInList.id]?.get(i)!!.plus(1)
+                    )
                 }
             }
         }
@@ -113,8 +175,8 @@ fun day4Part2() {
     var minuteTheGuardSleep = 0
     var counter = 0
 
-    for ((guardIdIndex, minutesValues) in overlapMinutesArray.withIndex()) {
-        for ((minuteIndex, value) in minutesValues.withIndex()) {
+    for ((guardIdIndex, minutesValues) in overlappingMinutesArray) {
+        for ((minuteIndex, value) in minutesValues) {
             if (value > counter) {
                 counter = value
                 guardIdOfMostMinutes = guardIdIndex
@@ -123,11 +185,11 @@ fun day4Part2() {
         }
     }
 
-    println("\nMinute it overlaps the most: $guardIdOfMostMinutes")
+    println("\nGuard: $guardIdOfMostMinutes \nMinute it overlaps the most: $minuteTheGuardSleep ")
 
     result = minuteTheGuardSleep * guardIdOfMostMinutes
 
-    println("\nResult Day4Part1: $result")
+    println("\nResult Day4Part2: $result")
     println("Time in Millis: " + (System.currentTimeMillis() - startTime) + "\n")
 }
 
